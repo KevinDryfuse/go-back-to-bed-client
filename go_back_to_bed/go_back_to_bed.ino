4df1966b2bfa454cfbc1e31a66de3aafe12c43cb
Kevin Dryfuse <kevin@kevindryfuse.com>
2020-09-15 13:57:44 -0400

    20x4 LCD base setup

--- go_back_to_bed/go_back_to_bed.ino
+++ go_back_to_bed/go_back_to_bed.ino
@@ -1,8 +1,31 @@
+// LCD
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,20,4);
+
 void setup() {
   Serial.begin(9600);
+  lcd.init();  //initialize the lcd
+  lcd.backlight();  //open the backlight
+  lcd_print(1, "Line 1 1234567890123");
+  lcd_print(2, "Line 2 1234567890123");
+  lcd_print(3, "Line 3 1234567890123");
+  lcd_print(4, "Line 4 1234567890123");
 }
 
 void loop() {
-  Serial.println("Intial Commit");
+  Serial.println("20x4 LCD Base Setup");
   delay(10000);
+}
+
+void lcd_print(int row, String message) {
+  message = message.substring(0, 20);
+  lcd.setCursor(0,row - 1);
+  lcd.print(pad_with_spaces(message));
+}
+
+String pad_with_spaces(String output_to_pad) {
+  String empty_screen = "                    ";
+  int whitespace_size = empty_screen.length() - output_to_pad.length();
+  return output_to_pad + empty_screen.substring(0, whitespace_size);
 }
