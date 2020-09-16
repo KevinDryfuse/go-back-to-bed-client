88ba541144186757905c7652df9382b84b3b9515
Kevin Dryfuse <kevin@kevindryfuse.com>
2020-09-15 22:58:11 -0400

    Simple httpclient example hitting GET endpoint

--- go_back_to_bed/go_back_to_bed.ino
+++ go_back_to_bed/go_back_to_bed.ino
@@ -3,6 +3,21 @@
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27,20,4);
 
+// LED RING
+#include <Adafruit_NeoPixel.h>
+#define PIN 12
+#define STRIPSIZE 24
+#define MAX_BRIGHTNESS 100
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(STRIPSIZE, PIN, NEO_GRB + NEO_KHZ800);
+
 // WIFI
 #include <ArduinoHttpClient.h>
 #include <SPI.h>
@@ -16,10 +31,20 @@ char pass[] = SECRET_PASS;
 WiFiClient wifi;
 int status = WL_IDLE_STATUS;
 
+// API
+const char serverAddress[] = "go-back-to-bed.herokuapp.com";  // server address
+int port = 80;
+HttpClient client = HttpClient(wifi, serverAddress, port);
+
+
 void setup() {
   Serial.begin(9600);
   lcd.init();  //initialize the lcd
   lcd.backlight();  //open the backlight
+
+  strip.begin();
+  strip.setBrightness(100);
+  strip.show(); // Initialize all pixels to 'off'
 
   while (!Serial);
   while ( status != WL_CONNECTED) {
@@ -39,12 +64,63 @@ void setup() {
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
-
 }
 
 void loop() {
-  Serial.println("20x4 LCD Base Setup");
-  delay(10000);
+  // assemble the path for the GET message:
+  String path = "/api/v1/test";
+  client.sendHeader("Cache-Control", "no-cache");
+  
+  // send the GET request
+  Serial.println("making GET request");
+  client.get(path);
+
+  // read the status code and body of the response
+  int statusCode = client.responseStatusCode();
+  String response = client.responseBody();
+  Serial.print("Status code: ");
+  Serial.println(statusCode);
+  DynamicJsonDocument doc(2000);
+  deserializeJson(doc, response);
+  
+  String test_value = doc[F("test")];
+
+  Serial.print(response);
+  
+  // Print values.
+  Serial.print("test value: ");
+  Serial.println(test_value);
+
+  for (int pixel = 0; pixel < STRIPSIZE; pixel++) {
+    strip.setPixelColor(pixel, strip.Color(0,0,255));
+  }
+  strip.show();
+
+  for (int brightness = 0; brightness <= MAX_BRIGHTNESS; brightness++) {
+    strip.setBrightness(brightness);
+    for (int pixel = 0; pixel < STRIPSIZE; pixel++) {
+      strip.setPixelColor(pixel, strip.Color(0,0,255));
+    }
+    strip.show();
+    if (brightness == MAX_BRIGHTNESS) {
+      delay(100);
+    }
+    else {
+      delay(50);
+    }
+  }
+
+  for (int brightness = MAX_BRIGHTNESS; brightness >= 0; brightness--) {
+    strip.setBrightness(brightness);  
+    strip.show();
+    if (brightness == 0) {
+      delay(100);
+    }
+    else {
+      delay(50);
+    }
+  }
+  delay(1000);
 }
 
 void lcd_print(int row, String message) {
